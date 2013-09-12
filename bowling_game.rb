a3972558aec9ea56f2f8abdb37a04b8d6a361f75
MakerSquare Student <students@themakersquare.com>
2013-09-12 13:50:50 -0500

    Add passing test for score method

--- bowling_game.rb
+++ bowling_game.rb
@@ -3,4 +3,19 @@ class BowlingGame
   def initialize 
     @rolls = []
   end
+
+  # Record a roll in the game.
+  #
+  # pins - The integer number of pins knocked down in this roll.
+  #
+  # Returns nothing.
+  def roll(pins)
+    
+  end
+
+  # Returns the Ingeger score for this game. Expects to be run after all rolls
+  # for the game have been recorded
+  def score
+    0
+  end
 end
