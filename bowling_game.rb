353b4e1a5881e4096c41cd853c8d6bf0a4961baf
MakerSquare Student <students@themakersquare.com>
2013-09-12 14:32:42 -0500

    make the strike test pass

--- bowling_game.rb
+++ bowling_game.rb
@@ -20,15 +20,19 @@ class BowlingGame
     current_roll = 0
 
     while current_roll < @rolls.size - 1
-      roll = @rolls[current_roll]
+      roll      = @rolls[current_roll]
       next_roll = @rolls[current_roll + 1]
 
-      if roll + next_roll ==10
+      if roll == 10
+        total_score += 10 + next_roll + @rolls[current_roll + 2]
+        current_roll += 1
+      elsif roll + next_roll == 10
         total_score += 10 + @rolls[current_roll + 2]
+        current_roll += 2
       else
         total_score += roll + next_roll
-      end  
-      current_roll += 2
+        current_roll += 2
+      end 
     end
     return total_score
   end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -28,7 +28,7 @@ describe BowlingGame do
         expect(game.score).to eq(20)
       end 
     end
-    context "when a spare is throw" do
+    context "when a spare is thrown" do
       it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
         3.times { game.roll(5) }
         17.times { game.roll(0) }
@@ -36,5 +36,15 @@ describe BowlingGame do
         expect(game.score).to eq(20)
       end
     end
-  end      
+    context 'when a strike is thrown' do
+      it 'records the frame score as 10 plus the total number of pins knocked down in the next two rolls' do
+        game.roll(10)
+        game.roll(7)
+        game.roll(1)
+        16.times { game.roll(0) }
+
+        expect(game.score).to eq(26)
+      end
+    end  
+  end
 end
