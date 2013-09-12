ca6eb07fc2d9be47b0823788755982a620773889
MakerSquare Student <students@themakersquare.com>
2013-09-12 14:18:43 -0500

    Pass test for spare method

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -14,16 +14,27 @@ describe BowlingGame do
   end
 
   describe '#score' do
-    it "scores the game as 0 when only gutter balls are thrown" do
-      20.times { game.roll(0)}
+    context 'when only gutter balls are thrown' do
+      it "scores the game as 0" do
+        20.times { game.roll(0)}
 
-      expect(game.score).to eq(0)
+        expect(game.score).to eq(0)
+      end
     end
+    context "when one pin is knocked down on each roll" do
+      it "scores the game as 20" do
+        20.times {game.roll(1)}
 
-    it "scores the game as 20 when one pin is knocked down on each roll" do
-      20.times {game.roll(1)}
-
-      expect(game.score).to eq(20)
+        expect(game.score).to eq(20)
+      end 
     end
-  end
+    context "when a spare is throw" do
+      it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
+        3.times { game.roll(5) }
+        17.times { game.roll(0) }
+
+        expect(game.score).to eq(20)
+      end
+    end
+  end      
 end
