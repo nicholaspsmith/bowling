ded82f8f2824e422d934ef94e0c3ed7258843205
MakerSquare Student <students@themakersquare.com>
2013-09-12 20:50:26 -0500

    Begin implementing 10th frame scoring

--- bowling_game.rb
+++ bowling_game.rb
@@ -6,6 +6,8 @@ class BowlingGame
     @rolls = []
     @total_score = 0
     @current_roll = 0
+    @frame = 1
+    @last_frame = false
   end
 
   # Record a roll in the game.
@@ -20,8 +22,11 @@ class BowlingGame
   # Returns the Integer score for this game. Expects to be run after all rolls
   # for the game have been recorded
   def score
-    while @current_roll < @rolls.size - 1
+    while (@current_roll < @rolls.size - 1) && @frame <= 10
       init_roll
+      if @frame == 10
+        @last_frame = true
+      end
 
       if strike?
         score_strike
@@ -30,9 +35,12 @@ class BowlingGame
       else
         score_roll
       end 
+      @frame += 1
     end
+    
     return @total_score
   end
+ 
 
   private
 
@@ -55,6 +63,7 @@ class BowlingGame
   def score_strike
     @total_score += 10 + @next_roll + @rolls[@current_roll + 2]
     @current_roll += 1
+    @next_roll = -1
   end
 
   # Returns true if current roll is a spare, false otherwise
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -45,6 +45,55 @@ describe BowlingGame do
 
         expect(game.score).to eq(26)
       end
-    end  
+    end
+
+
+    context 'when a non-strike non-spare is thrown in 10th frame' do
+      it "does not let the bowler bowl again" do
+        20.times { game.roll(1)}
+        game.roll(8)
+        game.roll(8)
+        
+        expect(game.score).to eq(20)
+      end
+    end
+
+    context 'when a spare is thrown in 10th frame' do
+      it "lets the bowler bowl one more time" do
+        19.times { game.roll(1) }
+        game.roll(9)
+        game.roll(1)
+        game.roll(2)
+
+        expect(game.score).to eq(29)
+      end
+    end
+
+    context 'when a strike is thrown in 10th frame' do
+      it "lets the bowler bowl two more times and no more" do
+        19.times { game.roll(1) }
+        game.roll(10)
+        game.roll(7)
+        game.roll(10)
+        game.roll(10)
+        game.roll(10)
+
+        expect(game.score).to eq(36)
+      end
+    end
+
+    context 'when two fives are rolled in seperate frames' do
+      it "does not count as a spare" do
+        game.roll(3)
+        game.roll(5)
+        game.roll(5)
+        game.roll(3)
+        game.roll(3)
+        game.roll(3)
+
+        expect(game.score).to eq(22)
+      end
+    end
+
   end
 end
